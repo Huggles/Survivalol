9bec1cd88f47809519a7a161a721f47cfb0d71f5
Wouter van de Koppel <woutervdkoppel@gmail.com>
2020-11-02 18:58:06 +0100

    Improved main menu. Added pause menu.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public void ResignGame()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+
+    public void QuitGame()
+    {
+        UnityEngine.Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+}
